67e628b5232c8280589227e0a1304b546892de63
Aranel9966 <99angelo.dire@gmail.com>
2023-03-13 15:40:49 +0100

    add class

--- js/script.js
+++ js/script.js
@@ -13,8 +13,21 @@ const { createApp } = Vue
     data() {
       return {
         message: 'Ciao a tutti!',
-        image: 'https://wallpapercave.com/wp/wp3588300.jpg',
+        image: 'https://cdn.wallpapersafari.com/82/50/QRfT4t.jpg',
+        classColor:'center',
+        classWidth:'width-img'
       }
-    }
+    },
+    methods: {
+        switchimg(){
+            if(this.image=='https://cdn.wallpapersafari.com/82/50/QRfT4t.jpg' ){
+
+                this.image= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ8QhF4-H29qbK3zLHZ5L2SXAMIOp8DezSBBP9KifMURKWVDmtAOIJT97NVLyZxtUrWmw&usqp=CAU'
+            }else{
+                this.image= 'https://cdn.wallpapersafari.com/82/50/QRfT4t.jpg'
+            }
+        }
+
+    },
   }).mount('#app')
 
